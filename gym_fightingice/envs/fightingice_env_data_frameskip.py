1f1e9eaeaec393ae39a7062e5292e76133199ac6
Byron-Edwards <byron_edwards@outlook.com>
2020-07-21 11:09:47 +0800

    make 1 game 1000 rounds

--- gym_fightingice/envs/fightingice_env_data_frameskip.py
+++ gym_fightingice/envs/fightingice_env_data_frameskip.py
@@ -116,6 +116,7 @@ class FightingiceEnv_Data_Frameskip(gym.Env):
             self.java_env = subprocess.Popen(["java", "-Dsun.reflect.inflationThreshold=2147483647",
                                               "-cp", self.start_up_str,
                                               "Main", "--port", str(self.port), "--py4j", "--fastmode",
+                                              "-r", "1000",
                                             "--grey-bg", "--inverted-player", "1", "--mute", "--limithp", "400", "400"], stdout=devnull, stderr=devnull)
         elif self.system_name == "macos":
             self.java_env = subprocess.Popen(["java", "-XstartOnFirstThread", "-cp", self.start_up_str, "Main", "--port", str(self.port), "--py4j", "--fastmode",
